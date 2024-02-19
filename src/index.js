23b56b97258a586b74c1397ac12d42059a398dee
jpow10 <juliapowney@hotmail.co.uk>
2024-02-19 16:04:11 +0000

    Added JS alert

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
+function generatePoem(event) {
+  event.preventDefault();
+}
 
+alert("crafting your poem");
+
+let poemFormElement = documentQuerySelector("#poem-form");
+poemFormElement.addEventListener("submit", generatePoem);
