36d788a236dce7796045765f62bc55d2ad37bb01
jpow10 <juliapowney@hotmail.co.uk>
2024-02-20 12:09:53 +0000

    Added JS

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 function generatePoem(event) {
   event.preventDefault();
+
+  new Typewriter("#poem", {
+    strings: "Putting pen to paper, poetry in motion",
+    autoStart: true,
+    delay: 30,
+    cursor: "",
+  });
 }
 
-alert("crafting your poem");
-
-let poemFormElement = documentQuerySelector("#poem-form");
+let poemFormElement = document.querySelector("#poem-form");
 poemFormElement.addEventListener("submit", generatePoem);
