43a8bdf4b785e6aa3a36c57b2d7b5dfcd68453de
jpow10 <juliapowney@hotmail.co.uk>
2024-02-20 14:22:54 +0000

    Added API w/problems

--- src/index.js
+++ src/index.js
@@ -1,13 +1,24 @@
-function generatePoem(event) {
-  event.preventDefault();
+let apiURL = ""; // Declare apiURL outside the function to have access to it in multiple functions.
 
+function displayPoem(response) {
   new Typewriter("#poem", {
-    strings: "Putting pen to paper, poetry in motion",
+    strings: response.data.answer,
     autoStart: true,
     delay: 30,
     cursor: "",
   });
 }
+
+function generatePoem(event) {
+  event.preventDefault();
+
+  let apiKey = "b0aa3bbe4a50o046t63f82874a31fac6";
+  let prompt = "";
+  let context = "";
+  apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context={$context}&key={$apiKey}`;
+}
+
+axios.get(apiURL).then(displayPoem);
 
 let poemFormElement = document.querySelector("#poem-form");
 poemFormElement.addEventListener("submit", generatePoem);
