255a9a9ab832457a8b49c289ca6c5febf76566c9
jpow10 <juliapowney@hotmail.co.uk>
2024-02-21 12:03:50 +0000

    Issue fixed
    Axios was not in curly brackets.

--- src/index.js
+++ src/index.js
@@ -13,11 +13,10 @@ function generatePoem(event) {
   let instructionsInput = document.querySelector("#user-instructions");
   let apiKey = "b0aa3bbe4a50o046t63f82874a31fac6";
   let context = "Please generate a short poem about $(instructionsInput.value)";
-  let prompt = `User instructions. You are a romantic poem expert and love to write short poems. Your goal is to generate a 4 line poem in basic HTML and separate each line with a <br />. Make sure to follow the user instructions.`;
+  let prompt = `User instructions. You are a romantic poem expert and love to write short poems. Your goal is to generate only a 4 line poem in basic HTML and separate each line with a <br />. Make sure to follow the user instructions.`;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+  axios.get(apiUrl).then(displayPoem);
 }
-
-axios.get(apiUrl).then(displayPoem);
 
 let poemFormElement = document.querySelector("#poem-form");
 poemFormElement.addEventListener("submit", generatePoem);
